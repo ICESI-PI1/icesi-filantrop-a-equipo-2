cdb4991e43b1fe6688516465cf2e07da3fbff0a8
Yuluka Gigante <yulukeitor@gmail.com>
2023-09-11 17:18:31 -0500

    feat: create the project with django

---
+++ philantropy_student_monitoring_module/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for philantropy_student_monitoring_module project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'philantropy_student_monitoring_module.settings')
+
+application = get_asgi_application()
