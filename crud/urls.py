3d6ee1d399046f4554d5ccdeb86a6c7085e92953
JacoboOssa <jaco2419@gmail.com>
2023-09-18 08:50:58 -0500

    feat: added main view and log out option

--- crud/urls.py
+++ crud/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('signup/', views.signUp),
-    path('login/', views.logIn)
+    path('login/', views.logIn),
+    path('postlog/', views.postLog),
+     path('logout/', views.singout)
 ]
--- crud/views.py
+++ crud/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 
 
@@ -41,3 +41,10 @@ def logIn(request):
           else:
             login(request, user)
             return redirect( "/postlog")
+        
+def postLog(request):
+    return render(request,'logout.html')
+
+def singout(request):
+    logout(request) 
+    return redirect(home)
