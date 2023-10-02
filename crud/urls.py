809fdf74aa23c5d5e72b2a26b8eb5ce80f1b8ccb
Andrés Pino <pinoandres85@gmail.com>
2023-10-01 20:56:00 -0500

    feat: complete student register information function

--- crud/urls.py
+++ crud/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from . import views
 
@@ -7,5 +6,8 @@ urlpatterns = [
     path('signup/', views.signUp),
     path('login/', views.logIn),
     path('postlog/', views.postLog),
-    path('logout/', views.singout)
+    path('logout/', views.singout),
+    path('students_info/', views.guardar_estudiante, name='estudiante'),
+    path('students_confirm/', views.confirmacion, name='confirm'),
+    path('students_info/', views.guardar_estudiante, name='guardar_estudiante')
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.http import request
+from .models import Student
 
 
 # Create your views here.
@@ -30,7 +32,6 @@ def signUp(request):
 
             return HttpResponse('Contrasenas incorrectas')
 
-
 def logIn(request):
     if request.method == 'GET':
         return render(request, 'login.html', {
@@ -55,3 +56,39 @@ def postLog(request):
 def singout(request):
     logout(request)
     return redirect(home)
+
+def confirmacion(request):
+    return render(request, 'students_confirm.html')
+
+
+def guardar_estudiante(request):
+    if request.method == "POST":
+        idType = request.POST.get('tipo_documento')
+        idNumber = request.POST.get('numero_documento')
+        name = request.POST.get('nombre_completo')
+        email = request.POST.get('correo_electronico')
+        institutionalEmail = request.POST.get('correo_institucional')
+        icfesScore = request.POST.get('puntaje_icfes')
+        birthDate = request.POST.get('fecha_nacimiento')
+        cellphoneNumber = request.POST.get('numero_celular')
+        accumulatedAverage = request.POST.get('promedio_acumulado')
+        creditsStudied = request.POST.get('creditos_cursados')
+        genre = request.POST.get('genero')
+        studentCode = request.POST.get('codigo_identificador')
+
+        student = Student.objects.create(student_code=studentCode,
+                                         name=name,
+                                         genre=genre,
+                                         id_type=idType,
+                                         id_number=idNumber,
+                                         email=email,
+                                         institutional_email=institutionalEmail,
+                                         icfes_score=icfesScore,
+                                         birth_date=birthDate,
+                                         cellphone_number=cellphoneNumber,
+                                         accumulated_average=accumulatedAverage,
+                                         credits_studied=creditsStudied)
+        student.save()
+        print(Student.objects.all())
+        return render(request, 'students_info.html')
+    return render(request, 'students_info.html')
