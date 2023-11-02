1631d0316d6ac2255315fd76347d25f606f1a8e8
Yuluka Gigante <yulukeitor@gmail.com>
2023-11-02 15:17:42 -0500

    feat(report generation): add info replacement retaining text styles
    There was an error with the replacement of the information, as the styles of the text were being lost. Now, the information is being replaced (not all of it yet) while retaining the styles.

--- crud/views/send_report_to_donor.py
+++ crud/views/send_report_to_donor.py
@@ -30,8 +30,9 @@ def send_report_to_donor(request):
             student_testimony = request.POST.get('student-testimony', '')
 
             date = datetime.now().date()
+            semester = f'{date.year} - 1' if date.month < 6 else f'{date.year} - 2'
 
-            generate_general_report(date, selected_student, student_testimony)
+            generate_general_report(date, semester, selected_student, student_testimony)
 
             # print("Donante: {}".format(selected_donor))
             # print("Estudiante: {}".format(selected_student_id))
@@ -53,45 +54,48 @@ def send_report_to_donor(request):
     return render(request, 'send_report_to_donor.html')
 
 
-def generate_general_report(date, student, testimony):
-    semester = date
+"""
+Receives certain information, obtains the basic format of the report, and fills in 
+the available fields with the information received.
+"""
+def generate_general_report(date, semester, student, testimony):
     report_name = "Reporte general {} - {}.docx".format(student.student_code, semester)
     output_path = f'archivos/reportes/{report_name}'
     
     # Reads the format content
     format_content = read_report_format()
 
-    # Crear un nuevo documento de Word modificado
+    # Creates a new doc to add the modifications
     modified_doc = Document()
-
-    consolidated = ''
 
     for paragraph_info in format_content:
         new_paragraph = modified_doc.add_paragraph()
 
-        for run_info in paragraph_info['runs']:
-            # run_info['text'] = run_info['text'].replace("[Fecha]", str(date))
-            # run_info['text'] = run_info['text'].replace("[estudiante]", str(student.name))
-            # run_info['text'] = run_info['text'].replace("[semestre]", str(date))
-            # run_info['text'] = run_info['text'].replace("[testimonio]", testimony)
-            # run_info['text'] = run_info['text'].replace("[semestre]", str(date))
+        # Replaces the fields with the information received as parameters
+        paragraph_text = paragraph_info['paragraph_text']
+        paragraph_text = paragraph_text.replace("[Fecha]", semester)
+        paragraph_text = paragraph_text.replace("[estudiante]", str(student.name))
+        paragraph_text = paragraph_text.replace("[semestre]", semester)
+        paragraph_text = paragraph_text.replace("[testimonio]", testimony)
 
-            new_run = new_paragraph.add_run(run_info['text'])
-            new_run.font.size = Pt(run_info['font_size']) if run_info['font_size'] else None
-            new_run.font.bold = run_info['font_bold']
-            new_run.font.italic = run_info['font_italic']
-            new_run.font.name = run_info['font_name']
-            new_paragraph.alignment = run_info['alignment']
+        # Adds the modified text to the doc
+        new_paragraph.add_run(paragraph_text)
+        
+        for run in new_paragraph.runs:
+            for run_info in paragraph_info['runs']:
+                run.font.size = Pt(run_info['font_size']) if run_info['font_size'] else None
+                run.font.bold = run_info['font_bold']
+                run.font.italic = run_info['font_italic']
+                run.font.name = run_info['font_name']
+                new_paragraph.alignment = run_info['alignment']
 
-        new_paragraph.text = new_paragraph.text.replace("[Fecha]", str(date))
-        new_paragraph.text = new_paragraph.text.replace("[estudiante]", str(student.name))
-        new_paragraph.text = new_paragraph.text.replace("[testimonio]", testimony)
-        print(new_paragraph.text)
-
-    # Guardar el documento de Word modificado como PDF
+    # Saves the doc
     modified_doc.save(output_path)
 
 
+"""
+Reads a .docx document containing the basic format of the report to be generated. 
+"""
 def read_report_format():
     doc = Document('./crud/static/formats/Reporte general beneficiario.docx')
 
