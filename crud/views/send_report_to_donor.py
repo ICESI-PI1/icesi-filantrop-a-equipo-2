a8c036011843fd40f1fb2eb0e2c3e9ec37fd309f
Yuluka Gigante <yulukeitor@gmail.com>
2023-11-12 13:13:11 -0500

    na

--- crud/views/send_report_to_donor.py
+++ crud/views/send_report_to_donor.py
@@ -30,8 +30,9 @@ def send_report_to_donor(request):
             student_testimony = request.POST.get('student-testimony', '')
 
             date = datetime.now().date()
+            semester = str(f'{date.year} - {1}') if date.month <=6 else str(f'{date.year} - {2}')
 
-            generate_general_report(date, selected_student, student_testimony)
+            generate_general_report(date, semester, selected_student, student_testimony)
 
             # print("Donante: {}".format(selected_donor))
             # print("Estudiante: {}".format(selected_student_id))
@@ -53,8 +54,7 @@ def send_report_to_donor(request):
     return render(request, 'send_report_to_donor.html')
 
 
-def generate_general_report(date, student, testimony):
-    semester = date
+def generate_general_report(date, semester, student, testimony):
     report_name = "Reporte general {} - {}.docx".format(student.student_code, semester)
     output_path = f'archivos/reportes/{report_name}'
     
@@ -64,28 +64,24 @@ def generate_general_report(date, student, testimony):
     # Crear un nuevo documento de Word modificado
     modified_doc = Document()
 
-    consolidated = ''
-
     for paragraph_info in format_content:
         new_paragraph = modified_doc.add_paragraph()
 
-        for run_info in paragraph_info['runs']:
-            # run_info['text'] = run_info['text'].replace("[Fecha]", str(date))
-            # run_info['text'] = run_info['text'].replace("[estudiante]", str(student.name))
-            # run_info['text'] = run_info['text'].replace("[semestre]", str(date))
-            # run_info['text'] = run_info['text'].replace("[testimonio]", testimony)
-            # run_info['text'] = run_info['text'].replace("[semestre]", str(date))
+        # new_paragraph.text = new_paragraph.text.replace("[Fecha]", str(date))
+        # new_paragraph.text = new_paragraph.text.replace("[estudiante]", str(student.name))
+        # new_paragraph.text = new_paragraph.text.replace("[semestre]", semester)
+        # new_paragraph.text = new_paragraph.text.replace("[testimonio]", testimony)
 
+        for run_info in paragraph_info['runs']:
             new_run = new_paragraph.add_run(run_info['text'])
+
+            # new_run = new_paragraph.add_run(run_info['font_italic'])
             new_run.font.size = Pt(run_info['font_size']) if run_info['font_size'] else None
             new_run.font.bold = run_info['font_bold']
             new_run.font.italic = run_info['font_italic']
             new_run.font.name = run_info['font_name']
             new_paragraph.alignment = run_info['alignment']
 
-        new_paragraph.text = new_paragraph.text.replace("[Fecha]", str(date))
-        new_paragraph.text = new_paragraph.text.replace("[estudiante]", str(student.name))
-        new_paragraph.text = new_paragraph.text.replace("[testimonio]", testimony)
         print(new_paragraph.text)
 
     # Guardar el documento de Word modificado como PDF
