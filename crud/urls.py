3442d7c3642f2aa67dec8a1bb6162da05e4147d4
Yuluka Gigante <yulukeitor@gmail.com>
2023-11-13 00:33:57 -0500

    feat: add preview and report email sending
    Related with the preview, when the report is generated, a button is displayed in the screen, and the user can download it to see if the report is ok. Once he/she decides that the report is ok, he/she can click ont a button that will send it to the email of the selected donor.
    
    Inorder to make the report attachment possible, it was necessary to modify the email sending function of request_info_update.py

--- crud/urls.py
+++ crud/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('api/getStudents/', api.get_students, name='get-students'),
     path('api/getDonors/', api.get_donors, name='get-donors'),
     path('create_donor/', donors.save_donor, name='save_donor'),
-    path('')
+    path('sendReport/', send_report_to_donor.send_report, name='send_report'),
 ]
 
--- crud/views/send_report_to_donor.py
+++ crud/views/send_report_to_donor.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from crud.models import *
@@ -8,11 +8,14 @@ from django.http import HttpResponse
 from reportlab.pdfgen import canvas
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings  # Importa la configuración de Django
-import os
 from datetime import datetime
 from docx import Document
 from docx.shared import Pt
 from docx2pdf import convert
+import pythoncom
+import os
+
+from crud.views.request_info_update import send_email
 
 
 @login_required
@@ -20,7 +23,6 @@ def send_report_to_donor(request):
     if request.method == 'POST':
         try:
             selected_donor_id = request.POST.get('donor-id', '')
-            selected_donor = Donor.objects.get(id=selected_donor_id)
 
             selected_student_id = request.POST.get('student-id', '')
             selected_student = Student.objects.get(id=selected_student_id)
@@ -49,22 +51,21 @@ def send_report_to_donor(request):
 
                 crea_assistance = "No se registran asistencias a monitorías."
 
-            generate_general_report(date, semester, selected_student, student_testimony, non_academic_activities, crea_assistance)
-
-            # print("Donante: {}".format(selected_donor))
-            # print("Estudiante: {}".format(selected_student_id))
-            # print("Mensaje email: {}".format(email_message))
-            # print("Testimonio: {}".format(student_testimony))
+            report_name = generate_general_report(date, semester, selected_student, student_testimony, non_academic_activities, crea_assistance)
 
             result_message = "Reporte generado y enviado con éxito"
             
         except Exception as e:
             print(f"Error: {e}")
 
+            report_name = None
             result_message = "Error en la generación y envío de reporte"
         
         return render(request, 'send_report_to_donor.html', {
-            "result_message" : result_message
+            "result_message" : result_message,
+            "generated_report" : report_name,
+            "selected_donor_id" : selected_donor_id,
+            "email_message" : email_message,
         })
 
 
@@ -72,47 +73,93 @@ def send_report_to_donor(request):
 
 
 """
+Manages the event when the user clicks the send button, confirming that she/he is in agreement by sending the generated report.
+"""
+@login_required
+def send_report(request):
+    try:
+        selected_donor_id = request.POST.get('donor-id2', '')
+        selected_donor = Donor.objects.get(id=selected_donor_id)
+
+        email_message = request.POST.get('email-message', '')
+
+        generated_report_name = request.POST.get('report-name', '')
+        # report_path = os.path.join(settings.STATIC_ROOT, 'reports', generated_report_name)
+        report_path = f'crud/static/reports/{ generated_report_name }'
+
+        subject = 'Reporte general de beneficiario'
+
+        send_email(selected_donor.email, email_message, subject, attachment_path=report_path)
+
+        return redirect('/sendReportToDonor/')
+
+    except Exception as e:
+        print(f'Error: {e}')
+
+        result_message = 'Error al enviar email al donante'
+
+        return render(request, 'send_report_to_donor.html', {
+            'email_sending_result_message': result_message,
+            "generated_report" : generated_report_name,
+            "selected_donor_id" : selected_donor_id,
+            "email_message" : email_message,
+        })
+
+
+"""
 Receives certain information, obtains the basic format of the report, and fills in 
 the available fields with the information received.
 """
 def generate_general_report(date, semester, student, testimony, non_academic_activities, crea_assistance):
-    report_name = "Reporte general {} - {}.docx".format(student.student_code, semester)
-    output_path = f'archivos/reportes/{report_name}'
-    output_path_pdf = f'archivos/reportes/{report_name.replace(".docx", ".pdf")}'
+    try:
+        # Calls CoInitialize 'cause there was throwing an exception related with this calling
+        pythoncom.CoInitialize()
 
-    
-    # Reads the format content
-    format_content = read_report_format()
+        report_name = "Reporte general {} - {}.docx".format(student.student_code, semester)
+        output_path = f'crud/static/reports/{report_name}'
+        output_path_pdf = f'crud/static/reports/{report_name.replace(".docx", ".pdf")}'
 
-    # Creates a new doc to add the modifications
-    modified_doc = Document()
-
-    for paragraph_info in format_content:
-        new_paragraph = modified_doc.add_paragraph()
-
-        # Replaces the fields with the information received as parameters
-        paragraph_text = paragraph_info['paragraph_text']
-        paragraph_text = paragraph_text.replace("[Fecha]", semester)
-        paragraph_text = paragraph_text.replace("[estudiante]", str(student.name))
-        paragraph_text = paragraph_text.replace("[semestre]", semester)
-        paragraph_text = paragraph_text.replace("[testimonio_estudiante]", testimony)
-        paragraph_text = paragraph_text.replace("[actividades_no_académicas]", str(non_academic_activities))
-        paragraph_text = paragraph_text.replace("[asistencia_monitorias]", str(crea_assistance))
-
-        # Adds the modified text to the doc
-        new_paragraph.add_run(paragraph_text)
         
-        for run in new_paragraph.runs:
-            for run_info in paragraph_info['runs']:
-                run.font.size = Pt(run_info['font_size']) if run_info['font_size'] else None
-                run.font.bold = run_info['font_bold']
-                run.font.italic = run_info['font_italic']
-                run.font.name = run_info['font_name']
-                new_paragraph.alignment = run_info['alignment']
+        # Reads the format content
+        format_content = read_report_format()
 
-    # Saves the .docx and .pdf of the report
-    modified_doc.save(output_path)
-    convert(output_path, output_path_pdf)
+        # Creates a new doc to add the modifications
+        modified_doc = Document()
+
+        for paragraph_info in format_content:
+            new_paragraph = modified_doc.add_paragraph()
+
+            # Replaces the fields with the information received as parameters
+            paragraph_text = paragraph_info['paragraph_text']
+            paragraph_text = paragraph_text.replace("[Fecha]", str(date))
+            paragraph_text = paragraph_text.replace("[estudiante]", str(student.name))
+            paragraph_text = paragraph_text.replace("[semestre]", semester)
+            paragraph_text = paragraph_text.replace("[testimonio_estudiante]", testimony)
+            paragraph_text = paragraph_text.replace("[actividades_no_académicas]", str(non_academic_activities))
+            paragraph_text = paragraph_text.replace("[asistencia_monitorias]", str(crea_assistance))
+
+            # Adds the modified text to the doc
+            new_paragraph.add_run(paragraph_text)
+            
+            for run in new_paragraph.runs:
+                for run_info in paragraph_info['runs']:
+                    run.font.size = Pt(run_info['font_size']) if run_info['font_size'] else None
+                    run.font.bold = run_info['font_bold']
+                    run.font.italic = run_info['font_italic']
+                    run.font.name = run_info['font_name']
+                    new_paragraph.alignment = run_info['alignment']
+
+        # Saves the .docx and .pdf of the report
+        modified_doc.save(output_path)
+        convert(output_path, output_path_pdf)
+
+        return output_path_pdf.split('/')[-1]
+    except Exception as e:
+        print(f'Error: {e}')
+
+    finally:
+        pythoncom.CoUninitialize()
+
 
 
 """
@@ -122,8 +169,6 @@ def read_report_format():
     doc = Document('./crud/static/formats/Reporte general beneficiario.docx')
 
     doc_content = []
-
-    accumulated_paragraph = ''
 
     for paragraph in doc.paragraphs:
         para_content = []
