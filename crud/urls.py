eae1ee0a99c04b9f48dacd02e9028ea8aaa485fe
JacoboOssa <jaco2419@gmail.com>
2023-11-21 00:52:57 -0500

    feature: send and process (delete) an Alert

--- crud/urls.py
+++ crud/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import login, save_student, request_info_update, upload_scholarship_data, upload_non_academic_report, estudiantes, upload_CREA_report, upload_academic_report, send_report_to_donor, api,donors, reportes, home
+from .views import login, save_student, request_info_update, upload_scholarship_data, upload_non_academic_report, estudiantes, upload_CREA_report, upload_academic_report, send_report_to_donor, api,donors, reportes, home, send_notifications
 
 
 urlpatterns = [
     path('', login.signin, name='signin'),
-    path('home/', home.contar_registers),
+    path('home/', home.contar_registers,),
     path('students_info/', save_student.guardar_estudiante, name='guardar_estudiante'),
     path('askInfoUpdate/', request_info_update.ask_info_update, name='ask_info_update'),
     path('estudiantes/', estudiantes.estudiantes),
@@ -20,6 +20,9 @@ urlpatterns = [
     path('create_donor/', donors.save_donor, name='save_donor'),
     path('sendReport/', send_report_to_donor.send_report, name='send_report'),
     path('list-Students/',home.listar_alumnos),
-    path('list-Donors/', home.listar_donantes)
+    path('list-Donors/', home.listar_donantes),
+    path('list-alerts/', home.listar_alertas),
+    path('eliminarAlerta/<int:id_alert>',home.delete, name='eliminarAlerta'),
+    path('send-notis/',send_notifications.crear_alerta, name='send-notis')
 ]
 
--- crud/views/home.py
+++ crud/views/home.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from crud.models import Student, Donor
+from crud.models import Student, Donor, Alerta
 from django.http import HttpRequest
 
 def listar_alumnos(request):
@@ -10,10 +10,20 @@ def listar_donantes(request):
     donantes=Donor.objects.all()  
     return render(request, "ListaDonantes.html",{"donantes": donantes})
 
+def listar_alertas(request):
+    alertas= Alerta.objects.all()
+    return render(request, "ListaAlertas.html",{"alertas": alertas})
+
 def contar_registers(request):
+    alertas= Alerta.objects.all()
     donantes=Donor.objects.all()  
     cantidad_donantes = donantes.count()
     alumnos=Student.objects.all()  
     cantidad_alumnos = alumnos.count()
-    return render (request, "home.html",{"cantidad_donantes": cantidad_donantes, "cantidad_alumnos": cantidad_alumnos})
+    return render (request, "home.html",{"cantidad_donantes": cantidad_donantes, "cantidad_alumnos": cantidad_alumnos, "alertas": alertas})
 
+def delete(request, id_alert):
+    alerta = Alerta.objects.get(pk=id_alert)
+    alerta.delete()
+    alertas = Alerta.objects.all()
+    return render(request, "home.html", {"alertas": alertas, "mensaje": 'OK'})
